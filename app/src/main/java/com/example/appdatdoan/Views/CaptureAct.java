e98eb1662ed40b014e0754daffff043851e7a747
DESKTOP-C70KB89\ASUS <20520735@gm.uit.edu.vn>
2023-02-17 18:50:51 +0700

    add views

---
+++ app/src/main/java/com/example/appdatdoan/Views/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.appdatdoan.Views;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
---
+++ app/src/main/java/com/example/appdatdoan/Views/FavoriteActivity.java
@@ -0,0 +1,17 @@
+package com.example.appdatdoan.Views;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.appdatdoan.R;
+
+public class FavoriteActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+
+}
